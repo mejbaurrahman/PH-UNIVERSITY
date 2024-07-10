1a7a29bf9ac236eac65b11c9b6ccbcab7938c8d6
unknown <mejbacsedu@gmail.com>
2024-07-10 21:28:46 +0600

    student

---
+++ src/modules/student/student.model.ts
@@ -0,0 +1,86 @@
+import { Schema, model } from "mongoose";
+import {
+  TGuardian,
+  TLocalGuardian,
+  TStudent,
+  TUserName,
+} from "./student.interface";
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<TGuardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuradianSchema = new Schema<TLocalGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const studentSchema = new Schema<TStudent>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ["male", "female"],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloogGroup: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuradianSchema,
+  profileImg: { type: String },
+  isActive: ["active", "blocked"],
+});
+
+export const StudentModel = model<TStudent>("Student", studentSchema);
---
+++ src/modules/student/student.service.ts
@@ -0,0 +1,23 @@
+import { StudentModel } from "./student.model";
+import { TStudent } from "./student.interface";
+
+const createStudentIntoDB = async (student: TStudent) => {
+  const result = await StudentModel.create(student);
+  return result;
+};
+
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+
+const getSingleStudentFromDB = async (id: string) => {
+  const result = await StudentModel.findOne({ id });
+  return result;
+};
+
+export const StudentServices = {
+  createStudentIntoDB,
+  getAllStudentsFromDB,
+  getSingleStudentFromDB,
+};
